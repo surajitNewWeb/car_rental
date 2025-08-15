050510805290081a4908b0b32ea86979922b0a56
surajitNewWeb <surajitsamantakultha@gmail.com>
2025-08-15 20:02:03 +0530

    update

--- admin/view_vehical.php
+++ admin/view_vehical.php
@@ -18,9 +18,10 @@
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
- <link href="assets/css/view.css" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <!-- custom css -->
+  <link href="assets/css/view.css" rel="stylesheet">
  
 
 </head>
@@ -63,7 +64,6 @@
                 <th>Fuel</th>
                 <th>Transmission</th>
                 <th>Price/Day</th>
-                <th>Description</th>
                 <th>Actions</th>
               </tr>
             </thead>
